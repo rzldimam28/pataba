fdc3f9d491e0412e195dfc3fc31a93a5548c4acb
Imam Rizaldi <rzldimam28@gmail.com>
2022-07-11 16:45:30 +0800

    add dinas

--- publication/dprd/routes_dprd.py
+++ publication/dprd/routes_dprd.py
@@ -9,87 +9,82 @@ from flask_login import current_user, login_required
 @app.route('/publikasi/dprd')
 def dprd():
   data = Dprd.query.filter_by(district_id=None).order_by(Dprd.tahun).all()
-  return render_template('dprd.html', data=data)
+  return render_template('dprd/dprd.html', data=data)
 
 # dprd (Kecamatan)
 @app.route('/publikasi/dprd/<int:district_id>')
 def dprd_kec(district_id):
   data = Dprd.query.filter_by(district_id=district_id).order_by(Dprd.tahun).all()
-  id_path = int(str(request.path)[-1])
   district_name = Districts.query.filter_by(id=district_id).first()
-  return render_template('dprd_kec.html', data=data, district_id=district_id, id_path=id_path, district_name=district_name)
+  return render_template('dprd/dprd_kec.html', data=data, district_id=district_id, district_name=district_name)
 
 # edit tabel
 @app.route('/publikasi/dprd/add', methods=['GET', 'POST'])
 @login_required
 def dprd_add():
-  if current_user.role == 'admin' or current_user.officer_of_agency == 2 or current_user.officer_of_agency == None:
+  if current_user.role == 'admin' or current_user.officer_of_agency == 2:
     form = FormDprd()
     if form.validate_on_submit():
       if form.district_id.data == 'None':
         form.district_id.data = eval(form.district_id.data)
       else:
         form.district_id.data = int(form.district_id.data)
-      if current_user.role == 'admin' or current_user.officer_of_district == form.district_id.data:
-        rows_to_create = Dprd(tahun=form.tahun.data,
-                              u1=form.u1.data,
-                               u2=form.u2.data,
-                               u3=form.u3.data,
-                               u4=form.u4.data,
-                               u5=form.u5.data,
-                               u6=form.u6.data,
-                               u7=form.u7.data,
-                               u8=form.u8.data,
-                               u9=form.u9.data,
-                               u10=form.u10.data,
-                               u11=form.u11.data,
-                               u12=form.u12.data,
-                               u13=form.u13.data,
-                               u14=form.u14.data,
-                               u15=form.u15.data,
-                               u16=form.u16.data,
-                               u17=form.u17.data,
-                               u18=form.u18.data,
-                               u19=form.u19.data,
-                               u20=form.u20.data,
-                               u21=form.u21.data,
-                               u22=form.u22.data,
-                               u23=form.u23.data,
-                               u24=form.u24.data,
-                               u25=form.u25.data,
-                               u26=form.u26.data,
-                               u27=form.u27.data,
-                               u28=form.u28.data,
-                               u29=form.u29.data,
-                               u30=form.u30.data,
-                               u31=form.u31.data,
-                               u32=form.u32.data,
-                               u33=form.u33.data,
-                               u34=form.u34.data,
-                               u35=form.u35.data,
-                               u36=form.u36.data,
-                               u37=form.u37.data,
-                               u38=form.u38.data,
-                               u39=form.u39.data,
-                               u40=form.u40.data,
-                               u41=form.u41.data,
-                               u42=form.u42.data,
-                               u43=form.u43.data,
-                               u44=form.u44.data,
-                               u45=form.u45.data,
-                               district_id=form.district_id.data
-                              )
-        db.session.add(rows_to_create)
-        db.session.commit()
-        flash('Table Edited!', category='success')
-        return redirect(url_for('dprd'))
-      else:  
-        flash('Unauthorized', category='danger')
-        return redirect(url_for('dprd_add'))
+      rows_to_create = Dprd(tahun=form.tahun.data,
+                            u1=form.u1.data,
+                              u2=form.u2.data,
+                              u3=form.u3.data,
+                              u4=form.u4.data,
+                              u5=form.u5.data,
+                              u6=form.u6.data,
+                              u7=form.u7.data,
+                              u8=form.u8.data,
+                              u9=form.u9.data,
+                              u10=form.u10.data,
+                              u11=form.u11.data,
+                              u12=form.u12.data,
+                              u13=form.u13.data,
+                              u14=form.u14.data,
+                              u15=form.u15.data,
+                              u16=form.u16.data,
+                              u17=form.u17.data,
+                              u18=form.u18.data,
+                              u19=form.u19.data,
+                              u20=form.u20.data,
+                              u21=form.u21.data,
+                              u22=form.u22.data,
+                              u23=form.u23.data,
+                              u24=form.u24.data,
+                              u25=form.u25.data,
+                              u26=form.u26.data,
+                              u27=form.u27.data,
+                              u28=form.u28.data,
+                              u29=form.u29.data,
+                              u30=form.u30.data,
+                              u31=form.u31.data,
+                              u32=form.u32.data,
+                              u33=form.u33.data,
+                              u34=form.u34.data,
+                              u35=form.u35.data,
+                              u36=form.u36.data,
+                              u37=form.u37.data,
+                              u38=form.u38.data,
+                              u39=form.u39.data,
+                              u40=form.u40.data,
+                              u41=form.u41.data,
+                              u42=form.u42.data,
+                              u43=form.u43.data,
+                              u44=form.u44.data,
+                              u45=form.u45.data,
+                              district_id=form.district_id.data
+                            )
+      db.session.add(rows_to_create)
+      db.session.commit()
+      flash('Table Edited!', category='success')
+      return redirect(url_for('dprd'))
   else:
     flash('Bukan Dinasmu', category='danger')
     return redirect(url_for('publikasi_page')) 
-  return render_template('dprd_add.html', form=form)
+  return render_template('dprd/dprd_add.html', form=form)
 
 # hapus record
 @app.route('/publikasi/dprd/delete/<int:id>')
@@ -97,13 +92,10 @@ def dprd_add():
 def dprd_delete(id):
   row_to_delete = Dprd.query.filter_by(id=id).first()
   if current_user.role == 'admin' or current_user.officer_of_agency == 2 or current_user.officer_of_agency == None:
-    if current_user.role == 'admin' or current_user.officer_of_district == row_to_delete.district_id:
-      db.session.delete(row_to_delete)
-      db.session.commit()
-      return redirect(url_for('dprd'))
-    else:
-      flash('Unauthorized', category='danger')
-      return redirect(url_for('dprd'))
+    db.session.delete(row_to_delete)
+    db.session.commit()
+    flash('Data Berhasil Dihapus', category='success')
+    return redirect(url_for('dprd'))
   else:
     flash('Bukan Dinasmu', category='danger')
     return redirect(url_for('dprd'))
--- publication/routes.py
+++ publication/routes.py
@@ -2,39 +2,43 @@ from publication import app, db
 from publication.models import Users
 from flask import render_template, flash, redirect, url_for, request
 from publication.forms import LoginForm
-from flask_login import login_user, logout_user
+from flask_login import login_user, logout_user, current_user
 
 # home page
 @app.route('/')
 @app.route('/home')
 def home_page():
-  return render_template('index_agi.html')
+  return render_template('index.html')
 
 # publication page
 @app.route('/publikasi')
 def publikasi_page():
-  return render_template('publikasi_agi.html')
+  return render_template('publikasi.html')
 
 # login
 @app.route('/login', methods=['GET', 'POST'])
 def login_page():
-  form = LoginForm()
-  if form.validate_on_submit():
-    attempted_user = Users.query.filter_by(email=form.email.data).first()
-    if attempted_user:
-      if attempted_user.check_password_correction(attempted_password=form.password.data):
-        login_user(attempted_user)
-        flash('You are login', category="success")
-        return redirect(url_for('home_page'))
+  if current_user.is_authenticated:
+    flash("You are already login", category='danger')
+    return redirect(url_for('home_page'))
+  else:
+    form = LoginForm()
+    if form.validate_on_submit():
+      attempted_user = Users.query.filter_by(email=form.email.data).first()
+      if attempted_user:
+        if attempted_user.check_password_correction(attempted_password=form.password.data):
+          login_user(attempted_user)
+          flash('Anda Berhasil Login', category="success")
+          return redirect(url_for('home_page'))
+        else:
+          flash('Password Salah', category="danger")
       else:
-        flash('password salah', category="danger")
-    else:
-      flash('usernome not found', category="danger")
-  return render_template('login_agi.html', form=form)
+        flash('Email Tidak DItemukan', category="danger")
+  return render_template('login.html', form=form)
 
 # logout
 @app.route('/logout')
 def logout_page():
   logout_user()
-  flash('You are now logged out', category="info")
+  flash('Anda Telah Keluar', category="info")
   return redirect(url_for('login_page'))
