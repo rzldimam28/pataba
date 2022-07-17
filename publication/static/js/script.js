2b9f4a445dd1a73f6d6ce345a1173fa676c3fa1b
agiaprila <agi.aprila26@gmail.com>
2022-07-17 23:36:07 +0800

    change publication page

--- publication/static/js/script.js
+++ publication/static/js/script.js
@@ -28,3 +28,19 @@ function exportTableToExcel(tableID, filename = ''){
       downloadLink.click();
   }
 }
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
